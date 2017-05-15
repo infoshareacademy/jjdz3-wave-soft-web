9bd980bb1608e51ae7a2713984d3dc6387ca3e65
piotrgy <piotr_gy@wp.pl>
2017-05-15 20:53:08 +0200

    Small test, that application from lib/.jar works.

--- com.infoshareacademy.wave-soft-web/src/main/java/hellojava.java
+++ com.infoshareacademy.wave-soft-web/src/main/java/hellojava.java
@@ -1,2 +1,11 @@
+import WaveSoftProgram.FindingPart;
+
 public class hellojava {
+
+    //Below is just for test of feature/JZ3W-26
+    //parts_ebay.json should be in src/main/resources
+    /*public static void main(String[] args) {
+        FindingPart part = new FindingPart();
+        part.findPartMenu("parts_ebay.json");
+    }*/
 }
