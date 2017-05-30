78e2d4ee569067df1b35a225cc21da7552365bec
piotrgy <piotr_gy@wp.pl>
2017-05-30 20:43:24 +0200

    Added relation to PersistencePart, getters and setters.

--- com.infoshareacademy.wave-soft-web/src/main/java/partsweb/PersistencePartCategory.java
+++ com.infoshareacademy.wave-soft-web/src/main/java/partsweb/PersistencePartCategory.java
@@ -4,6 +4,8 @@ package partsweb;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -13,5 +15,34 @@ public class PersistencePartCategory {
     @GeneratedValue
     private Long id;
     private String categoryName;
-    private List<PersistencePart> parts;
+
+    @OneToMany(mappedBy = "partCategory")
+    private List<PersistencePart> parts = new ArrayList<>();
+
+    public PersistencePartCategory() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public List<PersistencePart> getParts() {
+        return parts;
+    }
+
+    public void setParts(List<PersistencePart> parts) {
+        this.parts = parts;
+    }
 }
