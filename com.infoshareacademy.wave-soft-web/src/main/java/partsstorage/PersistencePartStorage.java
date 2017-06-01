919b4c209d07a705d4ceb98be2ac809dd4ecdc5e
piotrgy <piotr_gy@wp.pl>
2017-06-01 21:44:18 +0200

    Started creating a service for managing entities.

---
+++ com.infoshareacademy.wave-soft-web/src/main/java/partsstorage/PersistencePartStorage.java
@@ -0,0 +1,13 @@
+package partsstorage;
+
+
+import partsweb.PersistencePlaceInCar;
+
+import java.util.List;
+
+public class PersistencePartStorage {
+    public List<PersistencePlaceInCar> getAllPlaces(){
+        return null;
+
+    }
+}
