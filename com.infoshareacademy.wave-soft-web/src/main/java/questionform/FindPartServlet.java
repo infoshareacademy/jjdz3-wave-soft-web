1e2b5c9be9d3969c99fe4280deaf889d569d74a5
piotrgy <piotr_gy@wp.pl>
2017-06-01 21:38:33 +0200

    Started to create servlet for finding parts from DB.

---
+++ com.infoshareacademy.wave-soft-web/src/main/java/questionform/FindPartServlet.java
@@ -0,0 +1,27 @@
+package questionform;
+
+
+import WaveSoftProgram.parts.PartParser;
+import partsstorage.PartsImport;
+import partsstorage.PersistencePartStorage;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/findpart")
+public class FindPartServlet extends HttpServlet{
+    private PersistencePartStorage persistencePartStorage = new PersistencePartStorage();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        /*PartParser partParser = new PartParser("parts_ebay.json");
+
+        PartsImport partsImport = new PartsImport();
+        partsImport.partImport(partParser.getPlaceInCarList());*/
+        req.setAttribute("placeInCar", persistencePartStorage.getAllPlaces());
+    }
+}
