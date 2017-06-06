45be86c3444976bd1ee4668f70abdf8933394642
piotrgy <piotr_gy@wp.pl>
2017-06-06 21:49:56 +0200

    Started to implement getting from entity of car places.

--- com.infoshareacademy.wave-soft-web/src/main/java/partsstorage/PersistencePartStorage.java
+++ com.infoshareacademy.wave-soft-web/src/main/java/partsstorage/PersistencePartStorage.java
@@ -1,13 +1,27 @@
 package partsstorage;
 
 
+import WaveSoftProgram.parts.PlaceInCar;
 import partsweb.PersistencePlaceInCar;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 import java.util.List;
 
 public class PersistencePartStorage {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Transactional
     public List<PersistencePlaceInCar> getAllPlaces(){
-        return null;
+        List<PersistencePlaceInCar> persistencePlaceInCarList = em
+                .createQuery("select p from PersistencePlaceInCar p", PersistencePlaceInCar.class)
+                .getResultList();
+        return persistencePlaceInCarList;
 
     }
+
+
 }
