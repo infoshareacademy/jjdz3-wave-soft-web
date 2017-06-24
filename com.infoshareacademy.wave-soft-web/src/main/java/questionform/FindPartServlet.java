5e811a241487d6838b1ca681976f2cc4a867d8f2
Piotr Górny <piotr_gy@wp.pl>
2017-06-24 13:57:28 +0200

    Improve a servlet for displaying a drop down lists.

--- com.infoshareacademy.wave-soft-web/src/main/java/questionform/FindPartServlet.java
+++ com.infoshareacademy.wave-soft-web/src/main/java/questionform/FindPartServlet.java
@@ -1,9 +1,10 @@
 package questionform;
 
 
-import WaveSoftProgram.parts.PartParser;
-import partsstorage.PartsImport;
 import partsstorage.PersistencePartStorage;
+import partsweb.PersistencePart;
+import partsweb.PersistencePartCategory;
+import partsweb.PersistencePlaceInCar;
 
 import javax.enterprise.inject.Default;
 import javax.inject.Inject;
@@ -13,12 +14,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/findpart")
 public class FindPartServlet extends HttpServlet{
     @Inject
     @Default
     private PersistencePartStorage persistencePartStorage;
+    private List<PersistencePlaceInCar> placeInCar;
+    private List<PersistencePartCategory> partCategory;
+    private List<PersistencePart> part;
+
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -28,10 +34,32 @@ public class FindPartServlet extends HttpServlet{
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Long placeId = Long.parseLong(req.getParameter("singleplace"));
-        req.setAttribute("placeInCar", persistencePartStorage.choosePlace(placeId));
-        req.setAttribute("partCategory", persistencePartStorage.chooseCategory(placeId));
+        //Long placeId = null;
+        String placeInCar = req.getParameter("singleplace");
+        if (placeInCar != null && !placeInCar.isEmpty()){
+            Long placeId = Long.parseLong(placeInCar);
+            this.placeInCar = persistencePartStorage.chooseOnePlace(placeId);
+            req.setAttribute("placeInCar", this.placeInCar);
+            req.setAttribute("partCategory", persistencePartStorage.chooseCategory(placeId));
+        }
 
+        String partCategory = req.getParameter("singlecategory");
+        if (partCategory != null && !partCategory.isEmpty()){
+            Long categoryId = Long.parseLong(partCategory);
+            this.partCategory = persistencePartStorage.chooseOneCategory(categoryId);
+            req.setAttribute("placeInCar", this.placeInCar);
+            req.setAttribute("partCategory", this.partCategory);
+            req.setAttribute("parts", persistencePartStorage.choosePart(categoryId));
+        }
+
+        String part = req.getParameter("singlepart");
+        if (part != null && !part.isEmpty()){
+            Long partId = Long.parseLong(part);
+            this.part = persistencePartStorage.chooseOnePart(partId);
+            req.setAttribute("placeInCar", this.placeInCar);
+            req.setAttribute("partCategory", this.partCategory);
+            req.setAttribute("parts", this.part);
+        }
 
 
         req.getRequestDispatcher("findpart.jsp").forward(req, resp);
