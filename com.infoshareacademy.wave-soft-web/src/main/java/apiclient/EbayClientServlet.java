19d99f8a21b2832a39dfb5258e12a1248150680d
piotrgy <piotr_gy@wp.pl>
2017-07-27 14:14:52 +0200

    Implemented a call to eBay API.

--- com.infoshareacademy.wave-soft-web/src/main/java/apiclient/EbayClientServlet.java
+++ com.infoshareacademy.wave-soft-web/src/main/java/apiclient/EbayClientServlet.java
@@ -1,5 +1,8 @@
 package apiclient;
 
+
+import javax.enterprise.inject.Default;
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,11 +10,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
 @WebServlet("/ebaysearch")
 public class EbayClientServlet extends HttpServlet {
 
+    @Inject
+    @Default
+    private EbayClient ebayClient;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        String searchPhrase = req.getParameter("searchPhrase");
+        req.setAttribute("searchResultList", ebayClient.searchOnEbay(searchPhrase));
+        req.getRequestDispatcher("partsearchresults.jsp").forward(req, resp);
     }
 }
